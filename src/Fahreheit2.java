26764217369e6a3165f81db9c0bd9bbe06221ee5
MuresanS <mureshansara@gmail.com>
2017-03-23 01:38:40 +0200

    A doua varianta de transformare a gradelor Fahrenheit. Transformarea este din grade Celsius in grade Fahreheit.

---
+++ src/Fahreheit2.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Sara on 23/03/2017.
+ */
+public class Fahreheit2 {
+    public static void main(String[] args) {
+
+        int a;
+        a=SkeletonJava.readIntConsole("Grade Celsius:");
+
+        final float F=1.8f;
+        double b=((a*F)+32);
+        System.out.println("Transformare in grade Fahrenheit:"+b);
+    }
+
+}
