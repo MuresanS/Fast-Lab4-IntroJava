66dd5ee3f9712e6b1cf4daf347db9a8268306016
MuresanS <mureshansara@gmail.com>
2017-03-23 01:26:34 +0200

    O varianta de transformare a gradelor Fahrenheit in cele Celsius.

---
+++ src/Fahrenheit1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Sara on 23/03/2017.
+ */
+public class Fahrenheit1 {
+    public static void main(String[] args) {
+
+        int a;
+        a=SkeletonJava.readIntConsole("Grade Fahreheit:");
+
+        double b=((a-32)/1.8);
+        System.out.println("Acesta este rezultatul in grade Celsius:"+b);
+    }
+}
