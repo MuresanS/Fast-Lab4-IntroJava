d9f375cbf420ce37541fef5712d6f91a718074bc
MuresanS <mureshansara@gmail.com>
2017-03-24 00:17:24 +0200

    Media a doua numere.

---
+++ src/Average2Nums.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Sara on 23/03/2017.
+ */
+public class Average2Nums {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        a=SkeletonJava.readIntConsole("Introduce-ti un numar: ");
+        b=SkeletonJava.readIntConsole("Introduce-ti al doilea numar: ");
+
+        double average =((a+b)/2.0);
+        SkeletonJava.printConsole("Media numerelor este: "+average);
+
+
+    }
+}
+
+//nu uita sa dai - Commit Push
