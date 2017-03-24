0c1fedaa97e35f521da29e626d2c88e2bb207db6
MuresanS <mureshansara@gmail.com>
2017-03-24 10:07:39 +0200

    Se atribuie un numar fiecarei luni.

---
+++ src/luni.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Sara on 24/03/2017.
+ */
+public class luni {
+
+    public static void main(String[] args) {
+
+        int numar;
+        numar=SkeletonJava.readIntConsole("Introduce-ti un numar: ");
+
+        switch (numar){
+            case 1: System.out.println("Ianuarie");break;
+            case 2: System.out.println("Februarie");break;
+            case 3: System.out.println("Martie");break;
+            case 4: System.out.println("Aprilie");break;
+            case 5: System.out.println("Mai");break;
+            case 6: System.out.println("Iunie");break;
+            case 7: System.out.println("Iulie");break;
+            case 8: System.out.println("August");break;
+            case 9: System.out.println("Septembrie");break;
+            case 10: System.out.println("Octombrie");break;
+            case 11: System.out.println("Noiembrie");break;
+            case 12: System.out.println("Decembrie");
+            default: System.out.println("Valoare incorecta.");break;
+        }
+
+
+    }
+}
